22c9eeac0ebc2f1fce0674ac83be78c04e6f1ac0
nikolai_repin <koyan1997@yandex.ru>
2025-05-26 16:25:58 +0400

    .

--- src/main/java/searchengine/repository/PageRepository.java
+++ src/main/java/searchengine/repository/PageRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import searchengine.model.Index;
+import searchengine.model.Lemma;
 import searchengine.model.Page;
 import searchengine.model.SiteEntity;
+
+import java.util.List;
 
 @Repository
 public interface PageRepository extends JpaRepository<Page, Integer> {
